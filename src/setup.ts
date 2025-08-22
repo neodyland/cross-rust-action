df1d711560d5724b872c8f28eec60d0eb32ff673
googlefan256 <googlefan@neody.dev>
2025-08-22 18:40:13 +0900

    fix

--- src/setup.ts
+++ src/setup.ts
@@ -55,7 +55,6 @@ ${zigPath} cc -target ${values.zigtarget} $@`;
     const crossCargo = `#!${values.sh}
 export CC=${ccPath}
 ${Bun.which("cargo")} +${values.toolchain} --config 'target.${values.rusttarget}.linker="${ccPath}"' build --target ${values.rusttarget} $@`;
-    console.log(crossCargo);
     const crossCargoPath = path.join(bin, "cross-cargo-build");
     await Bun.write(crossCargoPath, crossCargo);
     await $`chmod +x ${crossCargoPath}`.quiet();
